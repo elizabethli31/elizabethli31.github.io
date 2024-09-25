cbc722b80806c16dd7a77994315ea47bdf668f4c
Elizabeth Li <elli@chegg.com>
2024-09-25 16:23:21 -0400

    adddddd

---
+++ index.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    setTimeout(() => {
+        const textElement = document.getElementById('jumpingText');
+        textElement.classList.add('stop-animation');
+    }, 10000); 
+};
